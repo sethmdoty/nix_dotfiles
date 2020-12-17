955dfad7271b628ae75a3eea9ac854e79fde486e
Seth Doty <seth.doty@objectpartners.com>
2020-12-16 22:41:23 -0600

    lets try yabai

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -13,7 +13,6 @@
     dock = { autohide = false; };
     trackpad.Clicking = true;
     trackpad.TrackpadThreeFingerDrag = false;
-
   };
 
   system = {
@@ -60,6 +59,52 @@
   # programs.fish.enable = true;
   # Enable various package settings
   #
+  ###########
+  services.skhd.enable = true;
+  services.skhd.skhdConfig = builtins.readFile ./conf.d/skhd.conf;
+
+  services.yabai = {
+    enable = true;
+    package = pkgs.yabai;
+    enableScriptingAddition = true;
+    config = {
+      window_border = "on";
+      window_border_width = 5;
+      active_window_border_color = "0xff81a1c1";
+      normal_window_border_color = "0xff3b4252";
+      focus_follows_mouse = "off";
+      mouse_follows_focus = "on";
+      window_placement = "second_child";
+      window_opacity = "off";
+      window_topmost = "on";
+      window_shadow = "float";
+      active_window_opacity = "1.0";
+      normal_window_opacity = "1.0";
+      split_ratio = "0.50";
+      auto_balance = "on";
+      mouse_modifier = "fn";
+      mouse_action1 = "move";
+      mouse_action2 = "resize";
+      layout = "bsp";
+      top_padding = 10;
+      bottom_padding = 10;
+      left_padding = 10;
+      right_padding = 10;
+      window_gap = 10;
+      external_bar = "all:26:0";
+    };
+
+    extraConfig = ''
+      # rules
+      yabai -m rule --add app='System Preferences' manage=off
+      yabai -m rule --add label="Finder" app="^Finder$" title="(Co(py|nnect)|Move|Info|Pref)" manage=off
+      yabai -m rule --add label="App Store" app="^App Store$" manage=off
+      yabai -m rule --add label="Calculator" app="^Calculator$" manage=off
+      yabai -m rule --add label="VirtualBox" app="^VirtualBox$" manage=off
+    '';
+  };
+
+  ########
   users.users.sethdoty.shell = pkgs.zsh;
   home-manager.users.sethdoty = {
     home.sessionVariables = { EDITOR = "emacsclient"; };
