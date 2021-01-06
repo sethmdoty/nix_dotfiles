80a5052c6f2a9f1df414808c80476ac566ba215b
Seth Doty <seth.doty@objectpartners.com>
2021-01-06 17:07:49 -0600

    update firefox

--- overlays/firefox/default.nix
+++ overlays/firefox/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "Firefox";
-  version = "84.0.1";
+  version = "84.0.2";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     name = "Firefox-${version}.dmg";
     url =
       "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${version}/mac/en-US/Firefox%20${version}.dmg";
-    sha256 = "159jkkyzhsq66c0fvq10yvaq03wahg8xxhhvrqj58r74qg4c93wy";
+    sha256 = "1xycm3v8ix1lg91qv3kwihqbvq877crx88jn9vqaczvzl1zrdbic";
   };
 
   meta = with stdenv.lib; {
