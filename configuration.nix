59016339220cf289ca91b98af08886a348ba121a
Seth Doty <seth.doty@objectpartners.com>
2021-02-07 15:22:48 -0600

    enable openGL

--- configuration.nix
+++ configuration.nix
@@ -47,6 +47,20 @@ in {
   services.autorandr.enable = true;
   services.xserver.modules = [ pkgs.xf86_input_wacom ];
   services.xserver.wacom.enable = true;
+  #Enable OpenGL
+  #Reference here https://nixos.wiki/wiki/Accelerated_Video_Playback
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
