42a5e50dfed29a1d046ae2f4c3e629c9a319e494
Seth Doty <seth.doty@objectpartners.com>
2021-03-16 22:54:11 -0500

    Remove capture template icon override

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -221,88 +221,6 @@
   :commands (doct))
 
 (after! org-capture
-  (defun org-capture-select-template-prettier (&optional keys)
-    "Select a capture template, in a prettier way than default
-  Lisp programs can force the template by setting KEYS to a string."
-    (let ((org-capture-templates
-           (or (org-contextualize-keys
-                (org-capture-upgrade-templates org-capture-templates)
-                org-capture-templates-contexts)
-               '(("t" "Task" entry (file+headline "" "Tasks")
-                  "* TODO %?\n  %u\n  %a")))))
-      (if keys
-          (or (assoc keys org-capture-templates)
-              (error "No capture template referred to by \"%s\" keys" keys))
-        (org-mks org-capture-templates
-                 "Select a capture template\n━━━━━━━━━━━━━━━━━━━━━━━━━"
-                 "Template key: "
-                 `(("q" ,(concat (all-the-icons-octicon "stop" :face 'all-the-icons-red :v-adjust 0.01) "\tAbort")))))))
-  (advice-add 'org-capture-select-template :override #'org-capture-select-template-prettier)
-
-  (defun org-mks-pretty (table title &optional prompt specials)
-    (save-window-excursion
-      (let ((inhibit-quit t)
-      (buffer (org-switch-to-buffer-other-window "*Org Select*"))
-      (prompt (or prompt "Select: "))
-      case-fold-search
-      current)
-        (unwind-protect
-      (catch 'exit
-        (while t
-          (erase-buffer)
-          (insert title "\n\n")
-          (let ((des-keys nil)
-          (allowed-keys '("\C-g"))
-          (tab-alternatives '("\s" "\t" "\r"))
-          (cursor-type nil))
-      ;; Populate allowed keys and descriptions keys
-      ;; available with CURRENT selector.
-      (let ((re (format "\\`%s\\(.\\)\\'"
-            (if current (regexp-quote current) "")))
-            (prefix (if current (concat current " ") "")))
-        (dolist (entry table)
-          (pcase entry
-            ;; Description.
-            (`(,(and key (pred (string-match re))) ,desc)
-             (let ((k (match-string 1 key)))
-         (push k des-keys)
-         ;; Keys ending in tab, space or RET are equivalent.
-         (if (member k tab-alternatives)
-             (push "\t" allowed-keys)
-           (push k allowed-keys))
-         (insert (propertize prefix 'face 'font-lock-comment-face) (propertize k 'face 'bold) (propertize "›" 'face 'font-lock-comment-face) "  " desc "…" "\n")))
-            ;; Usable entry.
-            (`(,(and key (pred (string-match re))) ,desc . ,_)
-             (let ((k (match-string 1 key)))
-         (insert (propertize prefix 'face 'font-lock-comment-face) (propertize k 'face 'bold) "   " desc "\n")
-         (push k allowed-keys)))
-            (_ nil))))
-      ;; Insert special entries, if any.
-      (when specials
-        (insert "─────────────────────────\n")
-        (pcase-dolist (`(,key ,description) specials)
-          (insert (format "%s   %s\n" (propertize key 'face '(bold all-the-icons-red)) description))
-          (push key allowed-keys)))
-      ;; Display UI and let user select an entry or
-      ;; a sub-level prefix.
-      (goto-char (point-min))
-      (unless (pos-visible-in-window-p (point-max))
-        (org-fit-window-to-buffer))
-      (let ((pressed (org--mks-read-key allowed-keys prompt)))
-        (setq current (concat current pressed))
-        (cond
-         ((equal pressed "\C-g") (user-error "Abort"))
-         ;; Selection is a prefix: open a new menu.
-         ((member pressed des-keys))
-         ;; Selection matches an association: return it.
-         ((let ((entry (assoc current table)))
-            (and entry (throw 'exit entry))))
-         ;; Selection matches a special entry: return the
-         ;; selection prefix.
-         ((assoc current specials) (throw 'exit current))
-         (t (error "No entry available")))))))
-    (when buffer (kill-buffer buffer))))))
-  (advice-add 'org-mks :override #'org-mks-pretty)
   ;;
   ;; Set my org file paths
   ;; 
@@ -311,28 +229,6 @@
   (setq +org-capture-central-project-notes-file "~/Sync/org/org-files/projects.org")
   (setq +org-capture-central-project-todo-file "~/Sync/org/org-files/projects.org")
   (setq +org-capture-links-file "~/Sync/org/org-files/links.org")
-
-  (defun +doct-icon-declaration-to-icon (declaration)
-    "Convert :icon declaration to icon"
-    (let ((name (pop declaration))
-          (set  (intern (concat "all-the-icons-" (plist-get declaration :set))))
-          (face (intern (concat "all-the-icons-" (plist-get declaration :color))))
-          (v-adjust (or (plist-get declaration :v-adjust) 0.01)))
-      (apply set `(,name :face ,face :v-adjust ,v-adjust))))
-
-  (defun +doct-iconify-capture-templates (groups)
-    "Add declaration's :icon to each template group in GROUPS."
-    (let ((templates (doct-flatten-lists-in groups)))
-      (setq doct-templates (mapcar (lambda (template)
-                                     (when-let* ((props (nthcdr (if (= (length template) 4) 2 5) template))
-                                                 (spec (plist-get (plist-get props :doct) :icon)))
-                                       (setf (nth 1 template) (concat (+doct-icon-declaration-to-icon spec)
-                                                                      "\t"
-                                                                      (nth 1 template))))
-                                     template)
-                                   templates))))
-
-  (setq doct-after-conversion-functions '(+doct-iconify-capture-templates))
 ;;
 ;; We have just been making things pretty.  I should probably make some actual templates
 ;; 
